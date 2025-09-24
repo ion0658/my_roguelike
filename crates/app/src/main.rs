ef18100653b60ee611ba142b7e944c3c3df383ea
IonOH <7760008+ion0658@users.noreply.github.com>
2025-09-25 00:33:31 +0900

    first commit

---
+++ crates/app/src/main.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
+struct Entity(u64);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, hello_world)
+        .run();
+}
